4dcbf7769c7c038b02ca2845fe2022fb5ecfe883
Clauciofds <115019744+Clauciofds@users.noreply.github.com>
2024-05-20 20:56:40 -0300

    Atualizado em 20/05/2024

---
+++ modulo_02/aulas/06-metodos_arrays_II/05_exercicio.ts
@@ -0,0 +1,23 @@
+/*
+    Crie uma função que percorra um array com nomes de arquivo.
+    A função deve verificar se possui so menos um arquivo com a extensão ".bat".
+    Caso exista, deverá exibir a mensagem "virus encontrado".
+*/
+
+const fileList = [
+    'ippD.txt',
+    'codj.txt',
+    'trojan.bat'
+]
+
+// const antVirus = fileList.some((f) =>{
+//     const fr = f.split('.')
+//     console.log(fr)
+//     return fr[1] === 'bat' ? 'VIRUS ENCONTRADO' : false
+
+const antVirus = fileList.some((f) =>{
+    // console.log(f)
+    return f.includes('.bat')
+})
+
+console.log(antVirus)
