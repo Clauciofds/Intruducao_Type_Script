2cf06c06f54d8e7ea13e6ce428913de144568f76
Clauciofds <115019744+Clauciofds@users.noreply.github.com>
2024-06-06 12:53:09 -0300

    Atualização das aulas com test ou it

--- modulo_03/aulas/01-testes_automatizados/soma.test.js
+++ modulo_03/aulas/01-testes_automatizados/soma.test.js
@@ -1,5 +1,21 @@
 const soma = require("./soma")
 
-test('deve verificar o resultado de um soma', () => {
-    expect(soma(3, 8)).toBe(11)
+describe("Testes de soma", () => {
+    test('deve verificar o resultado de um soma', () => {
+        expect(soma(3, 8)).toBe(11)
+    })
+
+    test('deve verificar o resultado inconrreto de uma soma', () => {
+        expect(soma(3, 8)).not.toBe(12)
+    })
+})
+
+describe("Testes de soma_2", () => {
+    it('deve verificar o resultado de um soma', () => {
+        expect(soma(3, 8)).toBe(11)
+    })
+
+    it('deve verificar o resultado inconrreto de uma soma', () => {
+        expect(soma(3, 8)).not.toBe(12)
+    })
 })
