9925bbab126d7a6727ec39afff8ae9aa8b7cf2b8
Clauciofds <115019744+Clauciofds@users.noreply.github.com>
2024-06-06 15:52:43 -0300

    Implementação da função divisão com os testes

--- modulo_03/aulas/01-testes_automatizados/soma.test.ts
+++ modulo_03/aulas/01-testes_automatizados/soma.test.ts
@@ -9,13 +9,3 @@ describe("Testes de soma", () => {
         expect(soma(3, 8)).not.toBe(12)
     })
 })
-
-describe("Testes de soma_2", () => {
-    it('deve verificar o resultado de um soma', () => {
-        expect(soma(3, 8)).toBe(11)
-    })
-
-    it('deve verificar o resultado inconrreto de uma soma', () => {
-        expect(soma(3, 8)).not.toBe(12)
-    })
-})
