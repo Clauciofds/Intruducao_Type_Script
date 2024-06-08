4d3aadcb03bc1aae6c7cc3d42bcc5f56412c0f5f
Clauciofds <115019744+Clauciofds@users.noreply.github.com>
2024-06-08 11:24:32 -0300

    Configuração da buil do projeto TS

--- modulo_03/aulas/01-testes_automatizados/min_max.ts
+++
@@ -1,5 +0,0 @@
-const arr = [885.9, 984.9, 444.9, 528.9, 528.9];
-const min = Math.min(...arr);
-const max = Math.max(...arr);
-console.log(`Menor valor: ${min}`); // 444.9
-console.log(`Maior valor: ${max}`); // 984.9
