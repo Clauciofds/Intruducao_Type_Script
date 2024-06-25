f25aea453c8b90fc040ffa48ac19e81018a01d46
Clauciofds <115019744+Clauciofds@users.noreply.github.com>
2024-06-25 19:55:05 -0300

    <impl> Implementação da edição de autores

--- modulo_03/aulas/07-api_rest_OO/src/routes.ts
+++ modulo_03/aulas/07-api_rest_OO/src/routes.ts
@@ -6,6 +6,9 @@ const routes = Router()
 const autorControlador = new AutorControlador()
 
 routes.get('/autores', autorControlador.listar)
+routes.get('/detalhar/:id', autorControlador.detalhar)
+routes.post('/autores', autorControlador.cadastrar)
+routes.put('/autores/:id', autorControlador.editar)
 
 
 export default routes
