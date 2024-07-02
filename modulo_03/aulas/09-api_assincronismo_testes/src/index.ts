976a265ab2b1475d4253546221fa0f7de513cea7
Clauciofds <115019744+Clauciofds@users.noreply.github.com>
2024-07-02 12:28:49 -0300

    Implementado teste e seus modelos

---
+++ modulo_03/aulas/09-api_assincronismo_testes/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    return res.json("Tudo certo")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Server started.')
+})
